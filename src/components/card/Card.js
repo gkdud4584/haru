7f41885d9e9f1a1e1e2460be873f99022778d5bb
minseol <blessoms2017@gmail.com>
2022-05-15 14:27:37 +0900

    게시글 작성

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,6 +5,29 @@ import  './Card.css';
 function Cards() {
     return (
         <div className='cards'>
+  <h1>Market</h1>
+            <ul className='cards__items'>
+                        <CardItem
+                        src='https://cdn.pixabay.com/photo/2015/07/05/10/18/tree-832079_960_720.jpg'
+                        text='Images'
+                        label='Mystery'
+                        alt="page3"
+
+                        />
+                        <CardItem
+                        src='https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_960_720.jpg'
+                        text='Experience Football on Top of the Himilayan Mountains'
+                        label='Adventure'
+                        alt="page4"
+
+                        />
+                        <CardItem
+                        src='https://cdn.pixabay.com/photo/2014/09/11/18/23/tower-bridge-441853_960_720.jpg'
+                        text='Ride through the Sahara Desert on a guided camel tour'
+                        label='Adrenaline'
+                        alt="page5"
+                        />   
+                    </ul>                               
             <h1>사업 좋은점</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
@@ -13,46 +36,33 @@ function Cards() {
                          src = "https://cdn.pixabay.com/photo/2014/09/11/18/23/tower-bridge-441853_960_720.jpg"
                          text = "Expore the hidden waterfall deep inside the Amazon Jungle"
                          label = 'Adventure'
-                         path='/services'
                          alt="page1"
                         />    
                         <CardItem 
                          src = "https://cdn.pixabay.com/photo/2012/08/06/00/53/bridge-53769_960_720.jpg"
                          text = "Travel through the Islands of Bali in a Private Cruise"
                          label = 'Luxury'
-                         path='/services'
                          alt="page2"
 
+                        />                                                    
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem 
+                         src = "https://cdn.pixabay.com/photo/2014/09/11/18/23/tower-bridge-441853_960_720.jpg"
+                         text = "Expore the hidden waterfall deep inside the Amazon Jungle"
+                         label = 'Adventure'
+                         alt="page1"
                         />    
-                    </ul>    
-                    <ul className='cards__items'>
-                        <CardItem
-                        src='https://cdn.pixabay.com/photo/2015/07/05/10/18/tree-832079_960_720.jpg'
-                        text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-                        label='Mystery'
-                        path='/services'
-                        alt="page3"
+                        <CardItem 
+                         src = "https://cdn.pixabay.com/photo/2012/08/06/00/53/bridge-53769_960_720.jpg"
+                         text = "Travel through the Islands of Bali in a Private Cruise"
+                         label = 'Luxury'
+                         alt="page2"
 
-                        />
-                        <CardItem
-                        src='https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_960_720.jpg'
-                        text='Experience Football on Top of the Himilayan Mountains'
-                        label='Adventure'
-                        path='/products'
-                        alt="page4"
-
-                        />
-                        <CardItem
-                        src='https://cdn.pixabay.com/photo/2014/09/11/18/23/tower-bridge-441853_960_720.jpg'
-                        text='Ride through the Sahara Desert on a guided camel tour'
-                        label='Adrenaline'
-                        path='/sign-up'
-                        alt="page5"
-
-                        />   
-                    </ul>    
+                        />                                                    
+                    </ul>                              
                 </div>
-            </div>            
+            </div>    
         </div>
     );
 }
--- src/components/write/write.js
+++ src/components/write/write.js
@@ -3,11 +3,14 @@ import './write.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
+
 function Write() {
   const [write, setWrite] = useState({
     title: '',
     content: ''
   })
+
+  const [view, setView] = useState([]);
 
   const getValue = e => {
     const { name, value } = e.target;
@@ -17,33 +20,62 @@ function Write() {
     })
     console.log(write);
   };  
+
+  // const submitReview = ()=>{
+  //   Axios.post('', {
+  //     title: write.title,
+  //     content: write.content
+  //   }).then(()=>{
+  //     alert('등록 완료!');
+  //   })
+  // };
+
   return (
 
     <div className="App">
       <h1>게시글 작성</h1>
+      <div className='movie-container'>
+      {view.map(element => 
+        <div key={element}>
+          <h2>{element.title}</h2>
+          <div>
+            {element.content}
+          </div>
+        </div>)}
+      </div>
 
       <div className='form-wrapper'>
         <input className="title-input" type='text' name='title' onChange={getValue} placeholder='제목' />
-        <CKEditor
+        <CKEditor name ="content"
           editor={ClassicEditor}
-          data=""
+          config={{
+            placeholder: "작성하시오"
+          }}
           onReady={editor => {
             // You can store the "editor" and use when it is needed.
-          //   console.log('Editor is ready to use!', editor);
-          // }}
-          // onChange={(event, editor) => {
-          //   const data = editor.getData();
-          //   console.log({ event, editor, data });
-          // }}
-          // onBlur={(event, editor) => {
-          //   console.log('Blur.', editor);
-          // }}
-          // onFocus={(event, editor) => {
-          //   console.log('Focus.', editor);
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({event,editor,data});
+            setWrite({
+              ...write,
+              content: data
+            })
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
           }}
         />
       </div>
-      <button className="submit-button">입력</button>
+      <button className="submit-button"
+      onClick={() => {
+        setView(view.concat({...write}));
+      }}
+      >입력</button>
     </div>
   );
 }
